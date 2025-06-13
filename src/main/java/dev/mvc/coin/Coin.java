e1fc4905fc588800cb3edee1c4e689087ada3cca
joonsoo1234 <junsu_01@naver.com>
2025-06-13 09:36:07 +0900

    refactor:엔티티에 @Column 추가 명시적 매핑 설정

--- src/main/java/dev/mvc/coin/Coin.java
+++ src/main/java/dev/mvc/coin/Coin.java
@@ -2,6 +2,7 @@ package dev.mvc.coin;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,26 +22,31 @@ public class Coin {
   @Id
   @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="coin_seq")
   @SequenceGenerator(name="coin_seq", sequenceName="COIN_SEQ", allocationSize=1)
+  @Column(name = "coin_no", updatable = false)
   private Long coin_no;
   
   /** 코인 이름 */
+  @Column(name = "coin_name", nullable = false)
   private String coin_name;
   
   /** 코인 가격 */
+  @Column(name = "coin_price", nullable = false)
   private Integer coin_price=0;
   
   /** 코인 생성일, sysdate 자동생성 */
-  private Date coin_date;
+  @Column(name = "coin_date", columnDefinition = "DATE", nullable = false)
+  private String coin_date;
   
   /** 코인 정보 */
   @Lob
+  @Column(name = "coin_info", nullable = false)
   private String coin_info="";
   
   public Coin() {
     
   }
   
-  public Coin(String coin_name, Date coin_date, int coin_price, String coin_info) {
+  public Coin(String coin_name, String coin_date, int coin_price, String coin_info) {
     this.coin_name=coin_name;
     this.coin_date=coin_date;
     this.coin_price=coin_price;
