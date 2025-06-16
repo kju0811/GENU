9615b07fb944669c4dfa074ab8e1af9c837695a6
joonsoo1234 <junsu_01@naver.com>
2025-06-16 18:15:09 +0900

    feat:코인변동시 기록테이블에 칼럼생성, CORS 설정, CRUD기능구현

--- src/main/java/dev/mvc/coinlog/CoinlogService.java
+++ src/main/java/dev/mvc/coinlog/CoinlogService.java
@@ -1,8 +1,28 @@
 package dev.mvc.coinlog;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CoinlogService {
-
+  @Autowired
+  CoinlogRepository coinlogRepository;
+  
+  /** 코인기록 저장, Create, INSERT~, UPDATE~ */
+  public void save(Coinlog coinlog) {
+    coinlogRepository.save(coinlog); 
+  }
+  
+  /** 코인기록 id에 해당하는 코인 삭제 */
+  public void deleteEntity(Long id) {
+    coinlogRepository.deleteById(id);
+  }
+  
+  /** 모든 레코드 출력 */
+  public List<Coinlog> find_all() {
+    return coinlogRepository.findAll();  // method/SQL 자동 생성
+  }
+  
 }
