28b60a8a2df71d4abc62e1be0d1d677221d67c08
joonsoo1234 <junsu_01@naver.com>
2025-06-17 15:55:08 +0900

    feat:coin put기능 추가

--- src/main/java/dev/mvc/coinlog/CoinlogService.java
+++ src/main/java/dev/mvc/coinlog/CoinlogService.java
@@ -1,9 +1,12 @@
 package dev.mvc.coinlog;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import dev.mvc.coin.Coin;
 
 @Service
 public class CoinlogService {
@@ -18,6 +21,11 @@ public class CoinlogService {
   /** 코인기록 id에 해당하는 코인 삭제 */
   public void deleteEntity(Long id) {
     coinlogRepository.deleteById(id);
+  }
+  
+  /** 코인기록 id에 해당하는 정보 반환 */
+  public Optional<Coinlog> find_by_id(Long id) {
+    return coinlogRepository.findById(id);  // method/SQL 자동 생성
   }
   
   /** 모든 레코드 출력 */
