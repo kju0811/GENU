7c7073e10eb40998fe840a72f6384685bef8e1be
joonsoo1234 <junsu_01@naver.com>
2025-06-17 15:55:08 +0900

    feat:coin put기능 추가

--- src/main/java/dev/mvc/coinlog/CoinlogController.java
+++ src/main/java/dev/mvc/coinlog/CoinlogController.java
@@ -1,11 +1,19 @@
 package dev.mvc.coinlog;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import dev.mvc.coin.Coin;
 
 @RequestMapping(value = "/coinlog")
 public class CoinlogController {
@@ -23,4 +31,34 @@ public class CoinlogController {
     coinlogService.save(coinlog);
     return ResponseEntity.ok().build();
   }
+  
+  /**
+   * 전체 목록
+   * GET 요청을 처리하여 모든 Entity 객체의 리스트를 반환
+   * http://localhost:9100/issue/find_all
+   * @return
+   */
+  @GetMapping(value = "/find_all")
+  public List<Coinlog> find_all() {
+    return coinlogService.find_all();
+  }
+  
+  /**
+   * DELETE 요청을 처리하여 특정 ID를 가진 Entity 객체를 삭제
+   * http://localhost:9100/coinlog/21
+   * @param id
+   * @return
+   */
+  @DeleteMapping(value = "/{coinlog_id}")
+  public ResponseEntity<Void> deleteEntity(@PathVariable("coinlog_id") Long id) {
+    if (coinlogService.find_by_id(id).isPresent()) { // Entity가 존재하면
+      coinlogService.deleteEntity(id); // 삭제
+      return ResponseEntity.ok().build(); // 성공적으로 삭제된 경우 200 반환
+    } else {
+      return ResponseEntity.notFound().build(); // 찾지 못한 경우 404 반환
+    }
+  }
+  
+
+  
 }
