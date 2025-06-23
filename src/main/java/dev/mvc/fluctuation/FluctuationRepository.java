ade11aebe8a9de3a2d72bf17793f5946e2e26de8
joonsoo1234 <junsu_01@naver.com>
2025-06-23 17:19:59 +0900

    fix:기사분석에 의한 변동 버그 수정

--- src/main/java/dev/mvc/fluctuation/FluctuationRepository.java
+++ src/main/java/dev/mvc/fluctuation/FluctuationRepository.java
@@ -7,11 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface FluctuationRepository extends JpaRepository<Fluctuation, Long> {
-
-  @Query(value = "SELECT f.news_no " +
-      "FROM fluctuation f " +
-      "JOIN coin c ON f.coin_no = c.coin_no " +
-      "WHERE f.fluctuation_date BETWEEN SYSDATE - 3 AND SYSDATE " +
-      "AND c.coin_no = :coin_no", nativeQuery = true)
-  List<Long> findByRdatePeriod(@Param("coin_no") Long coin_no);
+  // 현재~3일전 까지의 뉴스를 찾아 리스트 반환
+  @Query(value = """
+      SELECT f.fluctuation_no, f.news_no, f.coin_no, f.fluctuation_date
+      FROM fluctuation f
+      WHERE f.fluctuation_date BETWEEN SYSDATE - 3 AND SYSDATE
+        AND f.coin_no = :coin_no
+      """, nativeQuery = true)
+  List<Fluctuation> findByRdatePeriod(@Param("coin_no") Long coin_no);
 }
