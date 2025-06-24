25d4995d90491a7d1347672cb989ff767cad3955
kju <rlawldns60@gmail.com>
2025-06-24 16:38:46 +0900

    feat: 라우팅 주소 추가

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,8 @@ function Home() {
         <div style={{ padding: "2rem", textAlign: "center" }}>
         <h1>GENU</h1>
         <p>React + Vite 프로젝트가 정상 작동 중입니다.</p>
-        <Link to="/ai/news">llm 기사 생성</Link>
+        <Link to="/ai/news">llm 기사 생성</Link><br />
+        <Link to="/ai/newsfind">기사 보러가기</Link>
         </div>
   );
 }
