7ab23c01113e078b8b4b90db34f97299c1b11e37
joonsoo1234 <junsu_01@naver.com>
2025-06-24 12:46:38 +0900

    feat:출석체크 기능 추가

--- src/main/java/dev/mvc/attendance/AttendanceRepository.java
+++ src/main/java/dev/mvc/attendance/AttendanceRepository.java
@@ -1,7 +1,22 @@
 package dev.mvc.attendance;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
-
+  @Query(value="""
+      SELECT attendance_no, attendance_date, attendance_cnt, member_no 
+      FROM (
+        SELECT attendance_no, attendance_date, attendance_cnt, member_no 
+        FROM attendance 
+        WHERE member_no = :member_no 
+        ORDER BY attendance_date DESC 
+      )
+      WHERE ROWNUM = 1
+      """, nativeQuery = true)
+  Optional<Attendance> lastCheckedAttendance(@Param("member_no") Long member_no);
+    
 }
--- src/main/java/dev/mvc/attendance/AttendanceService.java
+++ src/main/java/dev/mvc/attendance/AttendanceService.java
@@ -1,5 +1,6 @@
 package dev.mvc.attendance;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,12 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dev.mvc.coinlike.CoinlikeRepository;
+import dev.mvc.member.Member;
+import dev.mvc.member.MemberService;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AttendanceService {
   private final AttendanceRepository attendanceRepository;
+  private final MemberService memberService;
   
   /** Create, INSERT~, UPDATE~ */
   public void save(Attendance attendance) {
@@ -33,6 +37,39 @@ public class AttendanceService {
   public List<Attendance> find_all() {
     return attendanceRepository.findAll();  // method/SQL 자동 생성
   }
+  
+  /** 출석 체크 메서드 */
+  public void checkToDate(Long member_no) {
+    Optional<Attendance> atd = attendanceRepository.lastCheckedAttendance(member_no);
+    System.out.println("atd -> "+ atd);
+    LocalDate today = LocalDate.now();
+    
+    if (atd.isEmpty()) { // 처음 저장
+      saveAttendance(member_no, 1, today);
+      
+    } else if (atd.get().getAttendance_date().isEqual(today)) {
+      System.out.println("이미 출석함");
+      
+    } else {
+      int newCnt = atd.get().getAttendance_cnt() + 1;
+      saveAttendance(member_no, newCnt, today);
+      System.out.println("이전 출석 있음, 오늘 안함");
+      
+    }
+  }
+  
+  /** 출석용 컬럼추가 */
+  private void saveAttendance(Long member_no, int cnt, LocalDate date) {
+    Member member = memberService.findByMember_no(member_no);
+    Attendance attendance = new Attendance();
+    attendance.setMember(member);
+    attendance.setAttendance_cnt(cnt);
+    attendance.setAttendance_date(date);
+    save(attendance);
+    System.out.println("save 완료 -> " + attendance);
+  }
+
+  
   
   
 }
