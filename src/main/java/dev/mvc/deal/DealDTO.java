113d8fc42f0dfc5645642ef7ef28aff83919b0c7
joonsoo1234 <junsu_01@naver.com>
2025-06-27 17:44:19 +0900

    feat:거래-돈 기능 구현, 예약 기능 추가

--- src/main/java/dev/mvc/deal/DealDTO.java
+++ src/main/java/dev/mvc/deal/DealDTO.java
@@ -1,6 +1,7 @@
 package dev.mvc.deal;
 
 import dev.mvc.coin.Coin;
+import dev.mvc.member.Member;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -13,22 +14,60 @@ public class DealDTO {
   @Data
   @AllArgsConstructor
   @NoArgsConstructor
-  public static class DealPay {
-      @NotNull(message = "투자 금액을 입력해주세요.")
-      @Min(value = 50, message = "투자 금액은 1 누렁 이상이어야 합니다.")
+  public static class DealBuyPay {
+      @NotNull(message = "원하는 매수 금액을 입력해주세요.")
+      @Min(value = 50, message = "매수 금액은 1 누렁 이상이어야 합니다.")
       private int price;
       
       @NotNull(message = "갯수를 입력해주세요.")
       @Min(value = 1, message = "갯수는 1개 이상이어야 합니다.")
       private int cnt;
       
+      private int type=1; // 매수
+      private int fee=0;
+      
       private Coin coin;
+      private Member member;
+      
+      public Deal toEntity() {
+        return Deal.builder()
+                .coin(coin)
+                .deal_price(price)
+                .deal_cnt(cnt)
+                .deal_type(type)
+                .deal_fee(fee)
+                .member(member)
+
+                .build();
+    }
+  }
+  
+  @Data
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class DealSellPay {
+      @NotNull(message = "원하는 매도 금액을 입력해주세요.")
+      @Min(value = 50, message = "매도 금액은 1 누렁 이상이어야 합니다.")
+      private int price;
+      
+      @NotNull(message = "갯수를 입력해주세요.")
+      @Min(value = 1, message = "갯수는 1개 이상이어야 합니다.")
+      private int cnt;
+      
+      private int type=2; // 매도
+      private int fee=0;
+      
+      private Coin coin;
+      private Member member;
 
       public Deal toEntity() {
         return Deal.builder()
                 .coin(coin)
                 .deal_price(price)
                 .deal_cnt(cnt)
+                .deal_type(type)
+                .deal_fee(fee)
+                .member(member)
 
                 .build();
     }
--- src/main/java/dev/mvc/deal/DealRepository.java
+++ src/main/java/dev/mvc/deal/DealRepository.java
@@ -1,7 +1,33 @@
 package dev.mvc.deal;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DealRepository extends JpaRepository<Deal, Long> {
 
+  @Query("SELECT COALESCE(SUM(d.deal_cnt), 0) "
+      + "from Deal d "
+      + "where d.member.member_no=:member_no and d.coin.coin_no=:coin_no and d.deal_type=1")
+  Integer getBuybyCnt(@Param("member_no") Long member_no, @Param("coin_no") Long coin_no);
+  
+  @Query("SELECT COALESCE(SUM(d.deal_cnt), 0) "
+      + "FROM Deal d "
+      + "WHERE d.member.member_no = :member_no "
+      + "AND d.coin.coin_no = :coin_no "
+      + "AND (d.deal_type = 2 OR d.deal_type = 4)")
+  Integer getSellbyCnt(@Param("member_no") Long member_no, @Param("coin_no") Long coin_no);
+  
+  @Query("SELECT d "
+      + "FROM Deal d "
+      + "WHERE d.deal_type = 3 AND d.coin.coin_no = :coin_no")
+  List<Deal> getType3(@Param("coin_no") Long coin_no);
+  
+  @Query("SELECT d "
+      + "FROM Deal d "
+      + "WHERE d.deal_type = 4 AND d.coin.coin_no = :coin_no")
+  List<Deal> getType4(@Param("coin_no") Long coin_no);
 }
