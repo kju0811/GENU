ff0a53e9a38c2f9c11701c218deae1405c7b336a
joonsoo1234 <junsu_01@naver.com>
2025-07-01 18:10:59 +0900

    feat: 알림 기능 구현 및 코인 가격 도달 시 SMS 발송

--- src/main/java/dev/mvc/notice/NoticeService.java
+++ src/main/java/dev/mvc/notice/NoticeService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import dev.mvc.coin.Coin;
+import dev.mvc.pay.Pay;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -12,8 +14,25 @@ import lombok.RequiredArgsConstructor;
 public class NoticeService {
   private final NoticeRepository noticeRepository;
   
-  /** Create, INSERT~, UPDATE~ */
+  /** UPDATE */
+  public void saveUpdate(Notice notice) {
+    noticeRepository.save(notice); 
+  }
+  /** Create, INSERT~ */
   public void save(Notice notice) {
+    int wp = notice.getNotice_price();
+    int cp = notice.getCoin().getCoin_price();
+//    System.out.println("wp ->" +wp);
+//    System.out.println("cp ->" +cp);
+    if (wp < cp) {
+//      System.out.println("원하는 값이 이상이되면.");
+      notice.setNotice_type(1);
+    } else if (wp > cp) {
+//      System.out.println("원하는 값이 이하가되면.");
+      notice.setNotice_type(0);
+    } else {
+      throw new RuntimeException("원하시는 금액과 현재가가 같습니다.");
+    }
     noticeRepository.save(notice); 
   }
   
@@ -31,4 +50,15 @@ public class NoticeService {
   public List<Notice> find_all() {
     return noticeRepository.findAll();  // method/SQL 자동 생성
   }
+  
+  /** member_no에 해당 하는 알림 반환 */
+  public List<Notice> getMemberNotice(Long member_no) {
+    return noticeRepository.getMemberNotice(member_no);
+  }
+  
+  /** coin_no에 해당 하고 발송되는 않은 알림 반환 */
+  public List<Notice> getPending(Long coin_no) {
+    return noticeRepository.getPending(coin_no);
+  }
+  
 }
