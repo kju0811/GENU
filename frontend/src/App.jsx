1a5e43716db6d956c02eba9d97f51215382e7808
kju <rlawldns60@gmail.com>
2025-07-03 17:32:48 +0900

    feat: Calendar 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import CoinRead from "./components/CoinRead";
 import CoinList from "./components/CoinList";
 import CoinCreate from "./components/CoinCreate";
 import TradeForm from "./components/TradeForm";
+import Schedule from "./pages/Calendar";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
         <Route path="/coin/find_all" element={<CoinList/>} />
         <Route path="/coin/create" element={<CoinCreate/>} />
         <Route path="/coin/deal" element={<TradeForm/>} />
+        <Route path="/calendar" element={<Schedule/>} />
         {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
       {/* <Footer /> */}
