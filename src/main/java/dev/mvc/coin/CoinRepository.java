8d04f467e9b4681a63706b2fb64095bd14ee132d
joonsoo1234 <junsu_01@naver.com>
2025-07-09 17:14:27 +0900

    feat:코인,정보 검색기능 구현

--- src/main/java/dev/mvc/coin/CoinRepository.java
+++ src/main/java/dev/mvc/coin/CoinRepository.java
@@ -1,8 +1,17 @@
 package dev.mvc.coin;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 public interface CoinRepository extends JpaRepository<Coin, Long> {
+
+  @Query("SELECT c FROM Coin c WHERE "
+      + "LOWER(c.coin_name) LIKE LOWER(CONCAT('%', :keyword, '%')) "
+      + "OR c.coin_info LIKE %:keyword%")
+  List<Coin> searchCoinNameOrInfo (@Param("keyword") String keyword);
 
 }
