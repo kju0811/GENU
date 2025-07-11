ee21888c14570b6facb698afc323f8ef7b6782e8
kju <rlawldns60@gmail.com>
2025-07-11 11:36:24 +0900

    refactor: chatOpen 컴포넌트화

--- frontend/src/components/chatCompnents.jsx
+++ frontend/src/components/chatCompnents.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useGlobal } from "./GlobalContext";
 import imgsrc from "../images/send.png"
 import botimg from "../images/genu.png"
+import chatopen from "../images/genu.png";
 
 const chatHeader = () => {
   const { close, setClose } = useGlobal();
@@ -33,4 +34,16 @@ const botImg = () => {
   );
 }
 
-export { chatHeader,chatButton,botImg };
+const ChatOpen = () => {
+  const { close, setClose } = useGlobal();
+  return(
+    <>
+    <div onClick={() => setClose(false)} style={{cursor:'pointer'}}>
+    <img src={chatopen} style={{ width:'6%',height:'10%', position:'fixed',right:'2%',bottom:'3%'}}/>
+    <span style={{ position:'fixed',right:'1%',bottom:'13%'}}>AI 챗봇 NURUNG2입니다</span>
+    </div>
+    </>
+  );
+}
+
+export { chatHeader, chatButton, botImg, ChatOpen };
