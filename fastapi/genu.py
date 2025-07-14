2666d97628bba2fe62ec1fa5e58e011c4f136c64
kju <rlawldns60@gmail.com>
2025-07-14 12:06:44 +0900

    feat: news생성 권한검증 기능추가

--- fastapi/genu.py
+++ fastapi/genu.py
@@ -6,6 +6,7 @@ import re
 import io
 import base64
 from datetime import datetime
+import jwt  
 
 from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
@@ -38,6 +39,9 @@ store = {} # dictionary
 
 app = FastAPI()
 
+load_dotenv()
+SECRET_KEY = os.getenv("jwt")
+
 # CORS 설정 (브라우저 스크립트용)
 app.add_middleware(
     CORSMiddleware,
@@ -46,7 +50,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 client = OpenAI()
 
 def makeimg(content,title):
@@ -76,48 +79,61 @@ async def news(request:Request):
     print("-> news 함수")
     data = await request.json()
     option1 = data.get("option1")
-    option2 = data.get("option2")
+    coin_cate = data.get("option2")
     option3 = data.get("option3")
     
-    # 2) 출력 스키마 & 출력 파서 설정
-    response_schemas = [
-        ResponseSchema(name="res", description="{'제목:뉴스 제목 / 내용:경제 뉴스 생성(1000~1200자) / 분석: 1,0'}")
-    ]
-    output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
-    format_instructions = output_parser.get_format_instructions()
-    prompt = PromptTemplate.from_template(
-        "{system}\n"
-        "{option1}과 카테고리 {option2}, 그리고 추가 사항 {option3}조건에 맞는 경제 뉴스를 생성해줘, 내용은 1000자이상 1200이하 사이에서 생성해줘, 호재는 1로 악재를 0으로 판별해줘"
-        "{format_instructions}"
-    )
+    jwtToken = request.headers.get("Authorization")
+    jwtToken = jwtToken.replace("Bearer ", "").strip()
+    
+    payload = jwt.decode(jwtToken, SECRET_KEY, algorithms=["HS512"])
+    
+    member_no = payload.get("member_no")
+    role = payload.get("role")
+    
+    if role == "ADMIN":
+        # 2) 출력 스키마 & 출력 파서 설정
+        response_schemas = [
+            ResponseSchema(name="res", description="{'제목:뉴스 제목 / 내용:경제 뉴스 생성(1000~1200자) / 분석: 1,0'}")
+        ]
+        output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+        format_instructions = output_parser.get_format_instructions()
+        prompt = PromptTemplate.from_template(
+            "{system}\n"
+            "{option1}과 카테고리 {coin_cate}, 그리고 추가 사항 {option3}조건에 맞는 경제 뉴스를 생성해줘, 내용은 5글자이하 사이에서 생성해줘, 호재는 1로 악재를 0으로 판별해줘"
+            "{format_instructions}"
+        )
 
-    inputs = {
-        "system": "경제뉴스를 생성하는 시스템이야",
-        "option1" : option1,
-        "option2" : option2,
-        "option3" : option3,
-        "format_instructions": format_instructions
-    }
-    pipeline = prompt | llm | output_parser
-    result = pipeline.invoke(inputs)
-    
-    print("-> result:", result)
-    
-    news_data = result.get("res")
-    title, content= news_data.split("/ 내용:")
-    contentpart, emotion = re.split(r"\s*/\s*분석:", content)
-    
-    title = title.replace("제목:", "").strip()
-    content = contentpart.strip()
-    emotion = emotion.strip() 
-    
-    oracle.newsinsert(
-        title,
-        content,
-        emotion
-    )
-    
-    makeimg(content, title)
+        inputs = {
+            "system": "경제뉴스를 생성하는 시스템이야",
+            "option1" : option1,
+            "coin_cate" : coin_cate,
+            "option3" : option3,
+            "format_instructions": format_instructions
+        }
+        pipeline = prompt | llm | output_parser
+        result = pipeline.invoke(inputs)
+        
+        print("-> result:", result)
+        
+        news_data = result.get("res")
+        title, content= news_data.split("/ 내용:")
+        contentpart, emotion = re.split(r"\s*/\s*분석:", content)
+        
+        title = title.replace("제목:", "").strip()
+        content = contentpart.strip()
+        emotion = emotion.strip() 
+        
+        oracle.newsinsert(
+            title,
+            content,
+            emotion,
+            coin_cate,
+            member_no
+        )
+        
+        #makeimg(content, title)
+    else:
+        result = "권한이 부족합니다"
     
     return result
 
@@ -130,33 +146,43 @@ async def summary(request:Request):
     
     #print('넘어온 데이터:',result)
     
-    # 2) 출력 스키마 & 출력 파서 설정
-    response_schemas = [
-        ResponseSchema(name="res", description="{'500자이하로 요약된 내용'}")
-    ]
-    output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
-    format_instructions = output_parser.get_format_instructions()
-    prompt = PromptTemplate.from_template(
-        "{system}\n"
-        "{result}의 내용을 500자이하로 요약해줘"
-        "{format_instructions}"
-    )
+    jwtToken = request.headers.get("Authorization")
+    jwtToken = jwtToken.replace("Bearer ", "").strip()
+    
+    payload = jwt.decode(jwtToken, SECRET_KEY, algorithms=["HS512"])
+    
+    role = payload.get("role")
+    
+    if role == "ADMIN":
+        # 2) 출력 스키마 & 출력 파서 설정
+        response_schemas = [
+            ResponseSchema(name="res", description="{'500자이하로 요약된 내용'}")
+        ]
+        output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+        format_instructions = output_parser.get_format_instructions()
+        prompt = PromptTemplate.from_template(
+            "{system}\n"
+            "{result}의 내용을 500자이하로 요약해줘"
+            "{format_instructions}"
+        )
 
-    inputs = {
-        "system": "글 요약하는 시스템이야",
-        "result" : result,
-        "format_instructions": format_instructions
-    }
-    pipeline = prompt | llm | output_parser
-    summary = pipeline.invoke(inputs)
-    
-    summary = summary['res'].strip()
-    
-    oracle.newssummary(
-        summary
-    )
-     
-    print("-> result", summary)
+        inputs = {
+            "system": "글 요약하는 시스템이야",
+            "result" : result,
+            "format_instructions": format_instructions
+        }
+        pipeline = prompt | llm | output_parser
+        summary = pipeline.invoke(inputs)
+        
+        summary = summary['res'].strip()
+        
+        oracle.newssummary(
+            summary
+        )
+        
+        print("-> result", summary)
+    else:
+        summary="잘못된 형식의 토큰입니다"
         
     return JSONResponse(content={"res": summary})
 
