56c74fead2ccd0cf56c5c27ec2f4f18f800d6902
kju <rlawldns60@gmail.com>
2025-07-21 17:20:35 +0900

    feat: 파일 업로드 추가

--- frontend/src/pages/Announce.jsx
+++ frontend/src/pages/Announce.jsx
@@ -18,25 +18,34 @@ function Announce() {
     const [title,setTitle] = useState('');
     const [content,setContent] = useState('');
     const navigate = useNavigate();
+    const [img, setImg] = useState(null);
 
     const member_no = userInfo?.member_no
     const role = userInfo?.role
+
+   const formData = new FormData();
+    formData.append(
+    'announce',
+    new Blob([JSON.stringify({
+        announcetitle: title,
+        announce_content: content,
+        member: {
+        member_no: member_no
+        }
+    })], { type: 'application/json' })
+    );
+    if (img) {
+    formData.append('file', img);
+    }
 
     const create =()=> {
         if (title.trim() != '' && content.trim() != '') {
         fetch(`http://${getIP()}:9093/announce/create`, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
                 'Authorization': jwt
             },
-            body: JSON.stringify({ 
-                "announcetitle":title,
-                "announce_content":content,
-                "member": {
-                    "member_no": member_no
-                    }
-                }),
+            body: formData
             })
             .then((response) => {
                 if (response.ok) {
@@ -52,7 +61,7 @@ function Announce() {
         } else {
             alert("제목과 내용을 입력해주세요")
         }
-        }
+    }
 
     return(
         <>
@@ -65,6 +74,16 @@ function Announce() {
                 <span>내용</span><br/>
                 <textarea style={{width:"100%",height:'80%', border:'1px solid gray', borderRadius: '5px',resize:'none'}} value={content} onChange={(e) => setContent(e.target.value)} ></textarea>
             </div>
+            <div>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={e => setImg(e.target.files[0])}
+              className="mt-1"
+              required
+            />
+            {img && <p className="mt-2 text-sm text-green-600">{img.name} 선택됨</p>}
+          </div>
             <button className="btn btn-accent" onClick={() => create(title,content)} >공지사항생성</button>
         </>
     );
