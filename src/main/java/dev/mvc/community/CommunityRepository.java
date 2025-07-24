de7af680ca8b063c5f03714e1fd0c721a65982de
kmj <endend7438@naver.com>
2025-07-24 10:11:12 +0900

    feat:커뮤니티 기능 추가

--- src/main/java/dev/mvc/community/CommunityRepository.java
+++ src/main/java/dev/mvc/community/CommunityRepository.java
@@ -1,9 +1,16 @@
 package dev.mvc.community;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import dev.mvc.coin.Coin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommunityRepository extends JpaRepository<Community, Long> {
-  // 필요하면 커스텀 쿼리 추가 가능
+  @Query("SELECT c FROM Community c WHERE c.coin.coin_no = :coinNo")
+  Page<Community> findByCoinNo(@Param("coinNo") Long coinNo, Pageable pageable);
 }
