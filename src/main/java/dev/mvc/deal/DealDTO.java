caa176cd01cea9102d2da5d9671da273c1b756fc
joonsoo1234 <junsu_01@naver.com>
2025-07-25 11:15:15 +0900

    feat:현코인자산 반환을 위한 DTO 생성

--- src/main/java/dev/mvc/deal/DealDTO.java
+++ src/main/java/dev/mvc/deal/DealDTO.java
@@ -10,79 +10,79 @@ import lombok.NoArgsConstructor;
 
 @Data
 public class DealDTO {
-  
+
   /** 매수 주문 DTO */
   @Data
   @AllArgsConstructor
   @NoArgsConstructor
   public static class DealBuyPay {
-      @NotNull(message = "원하는 매수 금액을 입력해주세요.")
-      @Min(value = 50, message = "매수 금액은 50 누렁 이상이어야 합니다.")
-      private Integer price;
-      
-      @NotNull(message = "갯수를 입력해주세요.")
-      @Min(value = 1, message = "갯수는 1개 이상이어야 합니다.")
-      private Integer cnt;
-      
-      private int type=1; // 매수
-      private int fee=0;
-      
-      private Coin coin;
-      private Member member;
-      
-      public Deal toEntity() {
-        return Deal.builder()
-                .coin(coin)
-                .deal_price(price)
-                .deal_cnt(cnt)
-                .deal_type(type)
-                .deal_fee(fee)
-                .member(member)
+    @NotNull(message = "원하는 매수 금액을 입력해주세요.")
+    @Min(value = 50, message = "매수 금액은 50 누렁 이상이어야 합니다.")
+    private Integer price;
 
-                .build();
+    @NotNull(message = "갯수를 입력해주세요.")
+    @Min(value = 1, message = "갯수는 1개 이상이어야 합니다.")
+    private Integer cnt;
+
+    private int type = 1; // 매수
+    private int fee = 0;
+
+    private Coin coin;
+    private Member member;
+
+    public Deal toEntity() {
+      return Deal.builder().coin(coin).deal_price(price).deal_cnt(cnt).deal_type(type).deal_fee(fee).member(member)
+
+          .build();
     }
   }
-  
+
   /** 매도 주문 DTO */
   @Data
   @AllArgsConstructor
   @NoArgsConstructor
   public static class DealSellPay {
-      @NotNull(message = "원하는 매도 금액을 입력해주세요.")
-      @Min(value = 50, message = "매도 금액은 50 누렁 이상이어야 합니다.")
-      private Integer price;
-      
-      @NotNull(message = "갯수를 입력해주세요.")
-      @Min(value = 1, message = "갯수는 1개 이상이어야 합니다.")
-      private Integer cnt;
-      
-      private int type=2; // 매도
-      private int fee=0;
-      
-      private Coin coin;
-      private Member member;
+    @NotNull(message = "원하는 매도 금액을 입력해주세요.")
+    @Min(value = 50, message = "매도 금액은 50 누렁 이상이어야 합니다.")
+    private Integer price;
 
-      public Deal toEntity() {
-        return Deal.builder()
-                .coin(coin)
-                .deal_price(price)
-                .deal_cnt(cnt)
-                .deal_type(type)
-                .deal_fee(fee)
-                .member(member)
+    @NotNull(message = "갯수를 입력해주세요.")
+    @Min(value = 1, message = "갯수는 1개 이상이어야 합니다.")
+    private Integer cnt;
 
-                .build();
+    private int type = 2; // 매도
+    private int fee = 0;
+
+    private Coin coin;
+    private Member member;
+
+    public Deal toEntity() {
+      return Deal.builder().coin(coin).deal_price(price).deal_cnt(cnt).deal_type(type).deal_fee(fee).member(member)
+
+          .build();
     }
   }
-  
+
   /** 호가창을 위한 DTO */
   @Data
   @AllArgsConstructor
   @NoArgsConstructor
   public static class OrderList {
-      private int deal_price;
-      private int total_cnt;
+    private int deal_price;
+    private int total_cnt;
   }
-  
-  
+
+  /** 보유 코인 자산 DTO */
+  @Data
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class MyAssetList {
+    private Long coin_no;
+    private String coin_img;
+    private int cnt;
+    private int total_price;
+    private int profitAmount;
+    private double profitPercentage;
+  }
+
 }
