3a6e04c0047e1d3720d0983ff5ad8f10899f01fd
joonsoo1234 <junsu_01@naver.com>
2025-08-01 16:17:18 +0900

    refactor:호스트변경

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000, // 우선 3000사용
-    host:true,
+    host: '0.0.0.0',
     proxy: {
       '/api': 'http://localhost:9093', // ← 백엔드서버에 맞게 변경
     },
